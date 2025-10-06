31426b6aac65370c179f83d4ec5d499b30660010
Tenderfeel <tenderfeel@gmail.com>
2025-10-06 16:15:09 +0900

    危局用エネミー設定仮

---
+++ data/deadyAssult.ts
@@ -0,0 +1,13 @@
+export default [
+  {
+    name: {
+      ja: "「冒涜者」",
+    },
+    weaknesses: [],
+    resistances: [],
+    reccomend: {
+      assistType: ["defensive"],
+      speciality: ["attack"],
+    },
+  },
+];
